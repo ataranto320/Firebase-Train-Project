f907a6e4eae70cba555c7b9bfbcbe291bffff559
Austin T <austin.taranto@gmail.com>
2019-06-04 15:51:13 -0400

    added firebase script with issues

---
+++ javascript.js
@@ -0,0 +1,22 @@
+// results for info inputed through add train form
+
+// results shown from add train input
+
+// firebase
+<script src="https://www.gstatic.com/firebasejs/6.1.0/firebase-app.js"></script>
+
+<script src="/__/firebase/init.js"></script>
+
+<script>
+var firebaseConfig = {
+    apiKey: "AIzaSyAVD7LjAlxiisN6OHobVpuvNjoYMy5wQOM",
+    authDomain: "train-schedule-82d22.firebaseapp.com",
+    databaseURL: "https://train-schedule-82d22.firebaseio.com",
+    projectId: "train-schedule-82d22",
+    storageBucket: "train-schedule-82d22.appspot.com",
+    messagingSenderId: "680763801753",
+    appId: "1:680763801753:web:6e69a62cd17c9608"
+  };
+
+firebase.initializeApp(firebaseConfig);
+</script>
